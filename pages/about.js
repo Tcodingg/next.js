bf96e024eb580745bac0936c75e6e209dc9163aa
DePrincio <princetlive@yahoo.com>
2021-07-16 14:38:08 -0400

    set up nav

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,7 @@
-
-
 export default function about() {
   return (
     <div>
-      <h1>about page</h1>
+      <h1>About page</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rerum maiores quisquam repellat temporibus, quia maxime impedit voluptatem illo dolorem.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rerum maiores quisquam repellat temporibus, quia maxime impedit voluptatem illo dolorem.</p>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,11 @@ export default function Home() {
   return (
    
     <div>
-      <h1>Homepage</h1>
+
+      <h1>Home page</h1>
        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rerum maiores quisquam repellat temporibus, quia maxime impedit voluptatem illo dolorem.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rerum maiores quisquam repellat temporibus, quia maxime impedit voluptatem illo dolorem.</p>
+
  </div>
   )
 }
