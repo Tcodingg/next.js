856c9a768ec785ea85e44a4d4f85ded362461a9d
DePrincio <princetlive@yahoo.com>
2021-07-16 16:46:25 -0400

    redirect 404 page to home page after 3 seconds

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,22 @@
-
+import Link from 'next/link'
+import {useEffect} from 'react'
+import {useRouter} from 'next/router'
 
 export default function error() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/')
+    }, 3000);
+  })
   return (
-    <div>
+    <div className='error'>
       <h1>404</h1>
       <p>Page not found</p>
+      <p>go back to <Link className='redirect' href='/'>Home Page </Link> </p>
+      
     </div>
   )
 }
